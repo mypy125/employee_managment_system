c7fdbce18ee96bca70bf218c3fff2a77bc237b58
mohito <mypywelcome@gmail.com>
2024-01-09 18:41:42 +0400

    add_user_seting

--- src/main/java/org/example/Program.java
+++ src/main/java/org/example/Program.java
@@ -9,10 +9,23 @@ public class Program {
     public static void main(String[] args) {
         AdminUser user = new AdminUser("gor","mkhitaryan","gor@gmail.com");
         BaseEmployee user2 = new BaseEmployee("sveta","isakova","svis@gmail.com");
-        EmployeeUser user3 = new EmployeeUser("sasha","emelyanov","vasil@mail.ru",250,"SM","1234");
+        EmployeeUser user3 = new EmployeeUser("sasha","emelyanov","vasil@mail.ru",1500,"SM","1234");
+
+        Department department1 = new Department("developer");
+        Department department2 = new Department("QA");
 
         EmployeeManagementSystem ems = new EmployeeManagementSystem();
+        ems.addDepartment(department1);
+        ems.addDepartment(department2);
         ems.addEmployee(user);
+        ems.addEmployee(user2);
+        ems.addEmployee(user3);
+        ems.assignEmployeeToDepartment("sasha","developer");
+        ems.assignEmployeeToDepartment("sveta","QA");
+
+        System.out.println(ems.getEmployeeCount());
+        ems.generateBasicReport();
+
 
     }
 }
